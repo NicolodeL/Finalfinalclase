9ffcdab893c39c8a042a9b313f204964a9621a80
NlodeL <nlodelop@myuax.com>
2024-05-08 11:17:38 +0200

    creo la clase DirectMessage extendiendo la clase tweet

--- src/main/java/DirectMessage.java
+++ src/main/java/DirectMessage.java
@@ -1,2 +1,8 @@
-public class DirectMessage {
+public class DirectMessage extends Tweet {
+    private UserAccount receiver;
+
+    public DirectMessage(String message, UserAccount sender, UserAccount receiver) {
+        super(message, sender);
+        this.receiver = receiver;
+    }
 }
