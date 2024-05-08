a85c295eeef7118983fe4487550e80d02cc413d2
NlodeL <nlodelop@myuax.com>
2024-05-08 10:45:36 +0200

    Hago el calculo de forma recursiva la suma de los dígitos de un número siendo un ejemplo el número 102 -> 1 + 0 + 2 = 3

--- src/main/java/Ej4.java
+++ src/main/java/Ej4.java
@@ -1,2 +1,15 @@
 public class Ej4 {
+    public static void main(String[] args) {
+        int num = 102;
+        int sum = sumOfDigits(num);
+        System.out.println(sum);
+    }
+
+    public static int sumOfDigits(int num) {
+        if (num == 0) {
+            return 0;
+        } else {
+            return num % 10 + sumOfDigits(num / 10);
+        }
+    }
 }
