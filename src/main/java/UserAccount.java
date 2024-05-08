f64a5f05726c081ca1f1246a456613482f764c34
NlodeL <nlodelop@myuax.com>
2024-05-08 11:01:38 +0200

    Finalmente cre la clase UserAccount

--- src/main/java/UserAccount.java
+++ src/main/java/UserAccount.java
@@ -1,3 +1,28 @@
+import java.util.*;
+
+public class UserAccount {
+    private String alias;
+    private String email;
+    private List<Tweet> tweets;
+    private List<Tweet> timeline;
+    private Set<UserAccount> followers;
+    private Set<UserAccount> following;
+
+    public UserAccount(String alias, String email) {
+        if (!Utils.isValidEmail(email)) {
+            throw new IllegalArgumentException("Invalid email");
+        }
+        if (!Utils.isValidAlias(alias)) {
+            throw new IllegalArgumentException("Invalid alias");
+        }
+        this.alias = alias;
+        this.email = email;
+        this.tweets = new ArrayList<>();
+        this.timeline = new ArrayList<>();
+        this.followers = new HashSet<>();
+        this.following = new HashSet<>();
+    }
+}
 
 
 
