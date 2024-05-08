eb31a375c0bc71ed61df6c9a5a72428535e23631
NlodeL <nlodelop@myuax.com>
2024-05-08 11:43:16 +0200

    introduzo metodos necesarios para el ej 3

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Utils {
     public static boolean isValidEmail(String email) {
         return email.contains("@");
@@ -5,5 +7,35 @@ public class Utils {
 
     public static boolean isValidAlias(String alias) {
         return alias.matches("[a-zA-Z0-9]+");
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String leerCadena() {
+        return scanner.nextLine();
+    }
+
+    public static int leerEntero() {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Por favor, introduzca un número entero válido.");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+
+    public static float leerFloat() {
+        while (!scanner.hasNextFloat()) {
+            System.out.println("Por favor, introduzca un número flotante válido.");
+            scanner.next();
+        }
+        return scanner.nextFloat();
+    }
+
+    public static double leerDouble() {
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Por favor, introduzca un número double válido.");
+            scanner.next();
+        }
+        return scanner.nextDouble();
     }
 }
